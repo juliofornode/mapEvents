4b6053c4db4fbeddac78c100134a867da67b25be
Julio <juliofornode@gmail.com>
2015-10-04 17:24:52 -0700

    changes to prod

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-maps',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://administrador:clavesecreta@ds029824.mongolab.com:29824/map-your-events',
 	assets: {
 		lib: {
 			css: [
